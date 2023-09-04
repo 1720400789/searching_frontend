1965e671cc384bc50a61b800e7f31e643f01554d
zjluvcn <hello@zj.cn>
2023-09-04 16:17:49 +0800

    聚合搜索平台前端项目搭建

---
+++ src/components/MyDivider.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="my-divider"></div>
+</template>
+
+<style scoped>
+.my-divider {
+  margin-bottom: 16px;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="index-page">
+    <a-input-search
+        v-model:value="searchText"
+        placeholder="请输入搜索关键词"
+        enter-button="搜索"
+        size="large"
+        @search="onSearch"
+    />
+    <MyDivider />
+    <a-tabs v-model:activeKey="activeKey" @change="onTabChange">
+      <a-tab-pane key="post" tab="文章">
+        <PostList :post-list="postList" />
+      </a-tab-pane>
+      <a-tab-pane key="picture" tab="图片">
+        <PictureList :picture-list="pictureList" />
+      </a-tab-pane>
+      <a-tab-pane key="user" tab="用户">
+        <UserList :user-list="userList" />
+      </a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script lang="ts">
+import MyDivider from "@/components/MyDivider.vue";
+</script>
